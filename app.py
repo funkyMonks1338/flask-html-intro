5fe4e77fe98593493b1d84afc301d2de34d94724
Funky Monk <dimbv5210@gmail.com>
2022-02-11 21:52:38 +0500

    Task 2

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
 
+PROMOTION_LINES = [
+    'Человечество вырастает из детства.',
+    'Человечеству мала одна планета.',
+    'Мы сделаем обитаемыми безжизненные пока планеты.',
+    'И начнем с Марса!',
+    'Присоединяйся!']
+
+
 app = Flask(__name__)
 
 
@@ -12,6 +20,11 @@ def root():
 @app.route('/index')
 def index():
     return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    return ''.join(f'<p>{line}</p>' for line in PROMOTION_LINES)
 
 
 if __name__ == '__main__':
