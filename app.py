27058dfbb7601582df7a162881dcf2a68cd341be
Funky Monk <dimbv5210@gmail.com>
2022-02-11 21:56:16 +0500

    Task 4

--- app.py
+++ app.py
@@ -46,5 +46,46 @@ def image_mars():
     """
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    url_img = url_for('static', filename='img/mars_1.gif')
+    url_style = url_for('static', filename='css/style.css')
+    return """
+    <!DOCTYPE html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Привет, Марс!</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="{}">
+    </head>
+    
+    <body>
+        <h1>Жди нас, Марс!</h1>
+        <img src="{}" alt="Гифка 'Марс'">
+        <div class="alert alert-dark" role="alert">
+            {}
+        </div>
+        <div class="alert alert-success" role="alert">
+            {}
+        </div>
+        <div class="alert alert-secondary" role="alert">
+            {}
+        </div>
+        <div class="alert alert-warning" role="alert">
+            {}
+        </div>
+        <div class="alert alert-danger" role="alert">
+            {}
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    </body>
+    """.format(url_style, url_img, *PROMOTION_LINES)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
